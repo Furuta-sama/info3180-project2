81772a04109eee10f9c5a80ae93f9d8243aef238
Furuta-sama <tadavebrown@gmail.com>
2021-04-26 22:59:32 -0500

    Further changes for Heroku app

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5accdb11b2c10a78d7c92c5fa102ea77fcd50c2058b00f6e'
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:password123@localhost/project2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xfkfcbbpdmtlvl:d9b7d00946507e9afc9a4fc0a949c8b3e95f491fe3c3a359f5b3688408f8a05b@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d8sb0lkluhhssr'
+HEROKU_POSTGRESQL_COPPER_URL='postgres://nnrgwpacawumhp:e541a82dd4eebc75fadb639602ecfab5e039a6530503bf6a870dabd95b4ea737@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d95d2efrttk5qn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SALT'] = 'r?fPfnryZfJ=M*aQxz$h2_F#!X@YR9nEB&f^SU3qRkVTt3WeP528BRYGthRZ7@8hT4Wqh'
 
